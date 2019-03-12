b205ec81d5d8e9ad35b41315ede570a910b71fa5
VictorLongberg <48484547+VictorLongberg@users.noreply.github.com>
2019-03-12 16:00:15 +0100

    arriveeventalphaomegalol

--- src/Lab5/Event/ArriveEvent.java
+++ src/Lab5/Event/ArriveEvent.java
@@ -1,6 +1,5 @@
 package Lab5.Event;
 
-
 import Lab5.Queue.EventQueue;
 import Lab5.State.SimState;
 
@@ -8,24 +7,30 @@ import Lab5.State.SimState;
  * Created by Tobias Heidlund on 2019-03-08.
  */
 public class ArriveEvent extends Event {
-    private Customer customer;
+	private Customer customer;
 
-    public ArriveEvent(int startTime, EventQueue queue, SimState simState, Customer customer) {
-        super(startTime, queue, simState);
-        this.customer = customer;
-    }
+	public ArriveEvent(int startTime, EventQueue queue, SimState simState, Customer customer) {
+		super(startTime, queue, simState);
+		this.customer = customer;
+	}
 
-    @Override
-    public void ExecuteEvent() {
+	@Override
+	public void ExecuteEvent() {
+		if () {
+		if (maxCustomers <= currentCustomers) {
 
-    }
+		} else {
+			missedCustomers += 1;
+		}
+		}
+	}
 
-    @Override
-    public double getStartTime() {
-        return startTime;
-    }
+	@Override
+	public double getStartTime() {
+		return startTime;
+	}
 
-    public Customer getCustomer(){
-        return customer;
-    }
+	public Customer getCustomer() {
+		return customer;
+	}
 }
