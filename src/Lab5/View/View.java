7a86154320a56ca56f94cb3a3210cdade7acd33e
Klas <klaman-8@student.ltu.se>
2019-03-12 19:46:28 +0100

    Update View.java

--- src/Lab5/View/View.java
+++ src/Lab5/View/View.java
@@ -30,7 +30,7 @@ public class View extends SimView {
 	public void update(Observable arg0, Object arg1) {
 		System.out.println("HÄNDELSER");
 		System.out.println("==========");
-		System.out.println(state.getTime() + " " + arg1 +); // INTE KLAR event name och customer behövs
+		System.out.println(state.getTime() + " " + arg1 +); // INTE KLAR event name och customer variabler behövs
 	}
 
 	public void start() {
@@ -42,17 +42,19 @@ public class View extends SimView {
 		System.out.println("MIN plocktid: " + state.getP_min());
 		System.out.println("MAX Plocktid: " + state.getP_max());
 		System.out.println("Slumpad seed: " + state.getSeed());
+		System.out.println("");
 
 	}
 
 	public void end() {
+		System.out.println("");
 		System.out.println("RESULTAT");
 		System.out.println("==========");
-		System.out.println("Av " + state.getMaxCustomers() + " kunder behandlades " + state.getCompletedCustomers()
+		System.out.println("1) Av " + state.getMaxCustomers() + " kunder behandlades " + state.getCompletedCustomers()
 				+ " medans " + state.getMissedCustomers() + " kunder missades");
-		System.out.println("Total ledig tid från " + state.getKassor() + " kassor blev " + state.getTimeWasted()
+		System.out.println("2) Total ledig tid från " + state.getKassor() + " kassor blev " + state.getTimeWasted()
 				+ ". Genomsnittlig ledig tid blev " + state.getTimeWasted() / state.getKassor());
-		System.out.println("Total kötid för " + state.getMaxCustomers() + " kunder blev " + state.getTotalQueueTime()
+		System.out.println("3) Total kötid för " + state.getMaxCustomers() + " kunder blev " + state.getTotalQueueTime()
 				+ ". Genomsnittlig ledig tid blev " + state.getTotalQueueTime() / state.getMaxCustomers());
 
 	}
