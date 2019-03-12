329084982076ae83971a77ec991365b57232cb2a
tobia <tobias199@gmail.com>
2019-03-12 17:17:30 +0100

    daf

--- src/Lab5/Event/StartEvent.java
+++ src/Lab5/Event/StartEvent.java
@@ -8,15 +8,17 @@ import Lab5.State.MarketState;
  * Created by Tobias Heidlund on 2019-03-08.
  */
 public class StartEvent extends Event{
+    MarketState marketState;
 
-    public StartEvent(int startTime, EventQueue queue, MarketState marketState) {
+    public StartEvent(double startTime, EventQueue queue, MarketState marketState) {
         super(startTime, queue);
+        this.marketState = marketState;
     }
 
     @Override
     public void ExecuteEvent() {
         Customer customer = new Customer();
-        ArriveEvent arriveEvent = new ArriveEvent(,queue,marketState,customer);
+        ArriveEvent arriveEvent = new ArriveEvent(marketState.getTime().getNextCustomer(),queue,marketState,customer);
         queue.addEvent(arriveEvent);
     }
 
