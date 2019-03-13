d7f2ba5fbf9068d41679882ab6f4bc3bc9702703
SimonHotDog <48484552+SimonHotDog@users.noreply.github.com>
2019-03-13 17:36:44 +0100

    Update optimize.java

--- src/Lab5/optimize.java
+++ src/Lab5/optimize.java
@@ -1,5 +1,6 @@
-package Lab5;
+	package Lab5;
 
+import Lab5.Event.CloseEvent;
 import Lab5.Event.StartEvent;
 import Lab5.Queue.EventQueue;
 import Lab5.State.MarketState;
@@ -15,6 +16,38 @@ import Lab5.View.View;
 public class optimize {
 	public static void main(String[] args) {
 		
+	}
+	
+	MarketState one() {
+		int kassor  = 2;
+		int maxCustomers = 7;
+		double arrivalSpeed = 3;
+		double P_min = 0.6;
+		double P_max = 0.9;
+		double B_min = 0.35;
+		double B_max = 0.6;
+		int seed = 13;
+
+	
+		EventQueue events = new EventQueue();
+		MarketState state = new MarketState(kassor,maxCustomers,arrivalSpeed,P_min,P_max,B_min,B_max,seed);
+
+        StartEvent startEvent = new StartEvent(0,events,state);
+		events.addEvent(startEvent);
+
+		CloseEvent closeEvent = new CloseEvent(8,events,state);
+		events.addEvent(closeEvent);
+
+		Simulator Market = new Simulator(null, events, state);
+		Market.start();
+		
+		return state;
+		
+	}
+	
+	
+	public int two(int maxCustomers, double arrivalSpeed, double P_min, double P_max, double B_min, double B_max, int seed) {
+		return 0;
 	}
 
 	void three() {
