292b942caf8e89b0ef7c393829c6da525498bc71
Klas <klaman-8@student.ltu.se>
2019-03-13 14:22:09 +0100

    Update View.java

--- src/Lab5/View/View.java
+++ src/Lab5/View/View.java
@@ -96,7 +96,7 @@ public class View extends SimView {
 		System.out.println("");
 		System.out.println("HÄNDELSER");
 		System.out.println("==========");
-		System.out.println("Tid \t Händelse \t Kund \t Öppet \t LedKas  LedKaT  Kunder  CKund   MKund   KKunder TKötid  KöL    Kö");
+		System.out.println("Tid \t Händelse \t Kund \t Öppet \t LedKas  LedKaT  Kunder  CKund   MKund   KKunder TKötid  KöL     Kö");
 		//System.out.println("0,00" +" " + "Start");
 	}
 /**
