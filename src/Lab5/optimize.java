c77501755433177682c6c6bf43e99eda3c080f32
SimonHotDog <48484552+SimonHotDog@users.noreply.github.com>
2019-03-14 13:18:14 +0100

    Update optimize.java

--- src/Lab5/optimize.java
+++ src/Lab5/optimize.java
@@ -1,117 +1,119 @@
-	package Lab5;
+//	package Lab5;
+//
+//import Lab5.Event.CloseEvent;
+//import Lab5.Event.StartEvent;
+//import Lab5.Queue.EventQueue;
+//import Lab5.State.MarketState;
+//import Lab5.View.View;
+//
+///**
+// * Creates new simulation
+// * 
+// * @author Victor Longberg, Tobias Heidlund, Simon Lundberg och Klas Mannberg.
+// * @version 0
+// * @since 2019-03-08
+// */
+//public class optimize {
+//	public static void main(String[] args) {
+//		int kassor  = 2;
+//		int maxCustomers = 7;
+//		double arrivalSpeed = 3;
+//		double P_min = 0.6;
+//		double P_max = 0.9;
+//		double B_min = 0.35;
+//		double B_max = 0.6;
+//		int seed = 13;
+//	}
+//	
+//	/**
+//	 * This method runs a simulation with fixed parameters and then returns the end state.
+//	 * 
+//	 * @return The end state
+//	 */
+//	MarketState one(int kassor, int maxCustomers, double arrivalSpeed, double P_min, double P_max, double B_min, double B_max, int seed) {
+//	
+//		EventQueue events = new EventQueue();
+//		MarketState state = new MarketState(kassor,maxCustomers,arrivalSpeed,P_min,P_max,B_min,B_max,seed);
+//
+//        StartEvent startEvent = new StartEvent(0,events,state);
+//		events.addEvent(startEvent);
+//
+//		CloseEvent closeEvent = new CloseEvent(8,events,state);
+//		events.addEvent(closeEvent);
+//
+//		Simulator Market = new Simulator(null, events, state);
+//		Market.start();
+//		
+//		return state;
+//		
+//	}
+//	
+//	//Work in progress
+//	public int two(int maxCustomers, double arrivalSpeed, double P_min, double P_max, double B_min, double B_max, int seed) {
+//		int best = one(0, maxCustomers, arrivalSpeed, P_min, P_max, B_min, B_max, seed).getMissedCustomers();
+//		int idealKass = 0;
+//		for(int i = 1; i < 100; i++ ) {
+//			int j = one(i, maxCustomers, arrivalSpeed, P_min, P_max, B_min, B_max, seed).getMissedCustomers();
+//			if (j < best) {
+//				best = j;
+//				idealKass = i; 
+//			}
+//			
+//		}
+//		
+//		
+//		return 0; //Temporary
+//	}
+//
+//	void three() {
+//		
+//	}
+//}
+ 
 
-import Lab5.Event.CloseEvent;
+package Lab5;
+
+import java.util.Random;
+
+import Lab5.Event.Event;
 import Lab5.Event.StartEvent;
 import Lab5.Queue.EventQueue;
 import Lab5.State.MarketState;
-import Lab5.View.View;
 
-/**
- * Creates new simulation
- * 
- * @author Victor Longberg, Tobias Heidlund, Simon Lundberg och Klas Mannberg.
- * @version 0
- * @since 2019-03-08
- */
+
+ 
 public class optimize {
-	public static void main(String[] args) {
-		int kassor  = 2;
-		int maxCustomers = 7;
-		double arrivalSpeed = 3;
-		double P_min = 0.6;
-		double P_max = 0.9;
-		double B_min = 0.35;
-		double B_max = 0.6;
-		int seed = 13;
-	}
-	
-	/**
-	 * This method runs a simulation with fixed parameters and then returns the end state.
-	 * 
-	 * @return The end state
-	 */
-	MarketState one(int kassor, int maxCustomers, double arrivalSpeed, double P_min, double P_max, double B_min, double B_max, int seed) {
-	
-		EventQueue events = new EventQueue();
-		MarketState state = new MarketState(kassor,maxCustomers,arrivalSpeed,P_min,P_max,B_min,B_max,seed);
-
-        StartEvent startEvent = new StartEvent(0,events,state);
-		events.addEvent(startEvent);
-
-		CloseEvent closeEvent = new CloseEvent(8,events,state);
-		events.addEvent(closeEvent);
-
-		Simulator Market = new Simulator(null, events, state);
-		Market.start();
-		
-		return state;
-		
-	}
-	
-	//Work in progress
-	public int two(int maxCustomers, double arrivalSpeed, double P_min, double P_max, double B_min, double B_max, int seed) {
-		int best = one(0, maxCustomers, arrivalSpeed, P_min, P_max, B_min, B_max, seed).getMissedCustomers();
-		int idealKass = 0;
-		for(int i = 1; i < 100; i++ ) {
-			int j = one(i, maxCustomers, arrivalSpeed, P_min, P_max, B_min, B_max, seed).getMissedCustomers();
-			if (j < best) {
-				best = j;
-				idealKass = i; 
-			}
-			
-		}
-		
-		
-		return 0; //Temporary
-	}
-
-	void three() {
-		
-	}
-}
- 
-/**
-package main;
-import java.util.Random;
-
-import generalDEDS.DEDS;
-import generalDEDS.EventQueue;
-import snabbkop.SnabbState;
-import snabbkop.SnabbEvents.StartEvent;
-
- 
-public class Optimize {
 	final private static int OPTIMALLOST = 5273; 
 	final private static int MAXCUSTOMERS = 1400;
-	final private static double LAMBDA = 2000.0;
-	final private static double PICKTIME1 = 0.45;
-	final private static double PICKTIME2 = 0.65;
-	final private static double PAYTIME1 = 0.2;
-	final private static double PAYTIME2 = 0.3;
+	final private static double ARRIVALSPEED = 2000.0;
+	final private static double P_MIN = 0.45;
+	final private static double P_MAX = 0.65;
+	final private static double B_MIN = 0.2;
+	final private static double B_MAX = 0.3;
 	final private static double CLOSINGTIME = 20.00;
-	final private static long SEED = 42;
+	final private static int SEED = 42;
 	
 	
-	private static int runOptimal(int cashiers, long seed) {
+	private static int runOptimal(int kassor, int seed) {
 		EventQueue eventQueue = new EventQueue();
-		SnabbState state = new snabbkop.SnabbState(cashiers, Optimize.LAMBDA,
-				Optimize.PICKTIME1, Optimize.PICKTIME2, Optimize.PAYTIME1,
-				Optimize.PAYTIME2, Optimize.MAXCUSTOMERS, Optimize.CLOSINGTIME, seed);
-		StartEvent startingEvent = new StartEvent(eventQueue);
-		DEDS simulator = new DEDS(eventQueue, state);
-		eventQueue.enqueue(startingEvent);
-		simulator.run();
+		MarketState state = new MarketState(kassor, optimize.MAXCUSTOMERS, optimize.ARRIVALSPEED,
+				optimize.P_MIN, optimize.P_MAX, optimize.B_MIN,
+				optimize.B_MAX, seed);
+		StartEvent startingEvent = new StartEvent(0,eventQueue, state);
+		Simulator simulator = new Simulator(null, eventQueue, state);
+		eventQueue.addEvent(startingEvent);
+		simulator.start();
 		
-		return state.getNMissed();
+		return state.getMissedCustomers();
 
 	}
 	
-	private int runSim(int cashiers, long seed) {
+	private int runSim(int cashiers, int seed) {
 		return runOptimal(cashiers, seed);
 		
 	}
 	
-	private int optimizeSeed(long seed) {
+	private int optimizeSeed(int seed) {
 		int lost;
 		for (int n = MAXCUSTOMERS; n > 0; n--) {
 			lost = runSim(n, seed);
@@ -122,7 +124,7 @@ public class Optimize {
 		return 1;
 	}
 	
-	private int binOptimizeSeed(long seed) {
+	private int binOptimizeSeed(int seed) {
 		int lost;
 		int lastcashiers = 0;
 		int cashiers = MAXCUSTOMERS / 2;
@@ -145,9 +147,9 @@ public class Optimize {
 		int lastHighest;
 		Random rng = new Random();
 		
-		highest = optimizeSeed(rng.nextLong());
+		highest = optimizeSeed(rng.nextInt());
 		while (count < 100) {
-			last = optimizeSeed(rng.nextLong());
+			last = optimizeSeed(rng.nextInt());
 			lastHighest = highest;	
 			highest = last > highest ? last : highest;
 			if (highest == lastHighest) {
@@ -161,16 +163,15 @@ public class Optimize {
 	
 
 	public static void main(String[] args) {
-		Optimize opt = new Optimize();
+		optimize opt = new optimize();
 		System.out.println("Max som ryms, M..........: " + MAXCUSTOMERS);
-		System.out.println("Ankomshastighet, lambda..: " + LAMBDA);
-		System.out.println("Plocktider, [P_min..Pmax]: [" + PICKTIME1 + ".." + PICKTIME2 +"]");
-		System.out.println("Betaltider, [K_min..Kmax]: [" + PAYTIME1 + ".." + PAYTIME2 + "]");
-		System.out.println("Fr�, f...................: " + SEED);
-		System.out.println("St�ngning sker " + CLOSINGTIME + " och stoph�ndelsen sker tiden 999.0.");
+		System.out.println("Ankomshastighet, lambda..: " + ARRIVALSPEED);
+		System.out.println("Plocktider, [P_min..Pmax]: [" + P_MIN + ".." + P_MAX +"]");
+		System.out.println("Betaltider, [K_min..Kmax]: [" + B_MIN + ".." + B_MAX + "]");
+		System.out.println("Frö, f...................: " + SEED);
+		System.out.println("Stängning sker " + CLOSINGTIME + " och stophändelsen sker tiden 999.0.");
 		System.out.println("Minst antal kassor som ger minimalt antal missade (" + OPTIMALLOST + "): " + opt.optimizeSeed(SEED));
 	}
 	
 	
 }
-*/
