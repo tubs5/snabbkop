1f6d1a117e8ff259cdcab5785d4b61e1353f28ab
SimonHotDog <48484552+SimonHotDog@users.noreply.github.com>
2019-03-14 15:08:09 +0100

    Update Time.java

--- src/Lab5/State/Time.java
+++ src/Lab5/State/Time.java
@@ -13,29 +13,60 @@ public class Time {
 	private UniformRandomStream payTime;
 	private ExponentialRandomStream arrivalSpeed;
 
+	/**
+	 * The Time constructor
+	 * 
+	 * @param state A MarketState
+	 */
 	public Time(MarketState state) {
+		//These uses the randomizers to get values
 		this.pickupTime = new UniformRandomStream(state.getP_min(), state.getP_max(), state.getSeed());
 		this.payTime = new UniformRandomStream(state.getB_min(), state.getB_max(), state.getSeed());
 		this.arrivalSpeed = new ExponentialRandomStream(state.getArrivalSpeed(), state.getSeed());
 
 	}
-
+	
+	/**
+	 * This method sets the time
+	 * 
+	 * @param time A double
+	 */
 	public void setTime(double time) {
 		this.time = time;
 	}
-
+	
+	/**
+	 * This method gets the time
+	 * 
+	 * @return the time
+	 */
 	public double getTime() {
 		return time;
 	}
-
+	
+	/**
+	 * This method gets the next pickup time
+	 * 
+	 * @return the next pickup time
+	 */
 	public double getNextPickupTime() {
 		return time + pickupTime.next();
 	}
-
+	
+	/**
+	 * This method gets the next paytime
+	 * 
+	 * @return the next paytime
+	 */
 	public double getNextPayTime() {
 		return time + payTime.next();
 	}
-
+	
+	/**
+	 * This method gets the next customer time
+	 * 
+	 * @return The next customer time
+	 */
 	public double getNextCustomer() {
 		return time + arrivalSpeed.next();
 	}
