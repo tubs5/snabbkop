0150eaa8a1861e85d6f5bcde7b6a07ff140c69c9
Klas <klaman-8@student.ltu.se>
2019-03-14 14:54:35 +0100

    format

--- src/Lab5/State/Time.java
+++ src/Lab5/State/Time.java
@@ -1,24 +1,24 @@
 package Lab5.State;
+
 /**
  * Creates random times for events
+ * 
  * @author Victor Longberg, Tobias Heidlund, Simon Lundberg och Klas Mannberg
  *
  */
 public class Time {
-	
+
 	private double time;
 	private UniformRandomStream pickupTime;
 	private UniformRandomStream payTime;
 	private ExponentialRandomStream arrivalSpeed;
 
-	public Time(MarketState state){
+	public Time(MarketState state) {
 		this.pickupTime = new UniformRandomStream(state.getP_min(), state.getP_max(), state.getSeed());
 		this.payTime = new UniformRandomStream(state.getB_min(), state.getB_max(), state.getSeed());
 		this.arrivalSpeed = new ExponentialRandomStream(state.getArrivalSpeed(), state.getSeed());
 
 	}
-
-
 
 	public void setTime(double time) {
 		this.time = time;
@@ -28,17 +28,16 @@ public class Time {
 		return time;
 	}
 
-	public double getNextPickupTime(){
-		return time+pickupTime.next();
+	public double getNextPickupTime() {
+		return time + pickupTime.next();
 	}
 
-	public double getNextPayTime(){
-		return time+payTime.next();
+	public double getNextPayTime() {
+		return time + payTime.next();
 	}
 
-	public double getNextCustomer(){
-		return time+arrivalSpeed.next();
+	public double getNextCustomer() {
+		return time + arrivalSpeed.next();
 	}
-
 
 }
