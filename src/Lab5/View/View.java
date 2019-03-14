05d1f4a000968f320f316ac6fb90a82872a0d3b6
Klas <klaman-8@student.ltu.se>
2019-03-14 12:53:16 +0100

    Update View.java

--- src/Lab5/View/View.java
+++ src/Lab5/View/View.java
@@ -30,46 +30,64 @@ public class View extends SimView {
 		super(update);
 		this.state = (MarketState) update;
 	}
-/**
- * 
- */
+
+	/**
+	 * 
+	 */
 	@Override
 	public void update(Observable o, Object arg1) {
 		String action = "Start";
 		int id = 0;
 		String open = (state.getStore()) ? "Ö" : "S";
-		double time = ((double)((int)(state.getTime().getTime()*100)))/100;
-		double timenoqueue = ((double)((int)(state.getTotalUnqueueTime()*100)))/100;
-		double timequeue = ((double)((int)(state.getTotalQueueTime()*100)))/100;
+		double time = ((double) ((int) (state.getTime().getTime() * 100))) / 100;
+		double timenoqueue = ((double) ((int) (state.getTotalUnqueueTime() * 100))) / 100;
+		double timequeue = ((double) ((int) (state.getTotalQueueTime() * 100))) / 100;
 
 		// beroende på typ av event behövs olika medelanden,
 		if (arg1 instanceof ArriveEvent) {
 			ArriveEvent event = (ArriveEvent) arg1;
 			id = event.getCustomer().getID();
 			action = "Ankomst";
-			System.out.println(time + " \t " + action +"\t "+id + " \t "+ open+ " \t " + (state.getKassor() - state.getActiveKassor())+ " \t " + timenoqueue + " \t " + state.getCurrentCustomers() + " \t " + state.getCompletedCustomers()+" \t "+ state.getMissedCustomers()+" \t " + state.getQueueingCustomers() + " \t "+ timequeue + " \t " + state.getFIFO().getSize() +" \t "+ state.getFIFO().toString());
+			System.out.println(time + " \t " + action + "\t " + id + " \t " + open + " \t "
+					+ (state.getKassor() - state.getActiveKassor()) + " \t " + timenoqueue + " \t "
+					+ state.getCurrentCustomers() + " \t " + state.getCompletedCustomers() + " \t "
+					+ state.getMissedCustomers() + " \t " + state.getQueueingCustomers() + " \t " + timequeue + " \t "
+					+ state.getFIFO().getSize() + " \t " + state.getFIFO().toString());
 
 		} else if (arg1 instanceof PayEvent) {
 			PayEvent event = (PayEvent) arg1;
 			id = event.getCustomer().getID();
 			action = "Betalning";
-			System.out.println(time + " \t " + action +" \t "+id +" \t "+ open+ " \t " + (state.getKassor() - state.getActiveKassor())+ " \t " + timenoqueue + " \t " + state.getCurrentCustomers() + " \t " + state.getCompletedCustomers()+" \t "+ state.getMissedCustomers()+" \t " + state.getQueueingCustomers() + " \t "+ timequeue + " \t " + state.getFIFO().getSize() +" \t "+ state.getFIFO().toString());
+			System.out.println(time + " \t " + action + " \t " + id + " \t " + open + " \t "
+					+ (state.getKassor() - state.getActiveKassor()) + " \t " + timenoqueue + " \t "
+					+ state.getCurrentCustomers() + " \t " + state.getCompletedCustomers() + " \t "
+					+ state.getMissedCustomers() + " \t " + state.getQueueingCustomers() + " \t " + timequeue + " \t "
+					+ state.getFIFO().getSize() + " \t " + state.getFIFO().toString());
 
 		} else if (arg1 instanceof PickupEvent) {
 			PickupEvent event = (PickupEvent) arg1;
 			id = event.getCustomer().getID();
 			action = "Plock";
-			System.out.println(time + " \t " + action +" \t  \t "+ id +" \t " + open+ " \t " + (state.getKassor() - state.getActiveKassor())+ " \t " + timenoqueue + " \t " + state.getCurrentCustomers() + " \t " + state.getCompletedCustomers()+" \t "+ state.getMissedCustomers()+" \t " + state.getQueueingCustomers() + " \t "+ timequeue + " \t " + state.getFIFO().getSize() +" \t "+ state.getFIFO().toString());
+			System.out.println(time + " \t " + action + " \t  \t " + id + " \t " + open + " \t "
+					+ (state.getKassor() - state.getActiveKassor()) + " \t " + timenoqueue + " \t "
+					+ state.getCurrentCustomers() + " \t " + state.getCompletedCustomers() + " \t "
+					+ state.getMissedCustomers() + " \t " + state.getQueueingCustomers() + " \t " + timequeue + " \t "
+					+ state.getFIFO().getSize() + " \t " + state.getFIFO().toString());
 		} else if (arg1 instanceof CloseEvent) {
-			//CloseEvent event = (CloseEvent) arg1;
+			// CloseEvent event = (CloseEvent) arg1;
 			action = "Close";
-			System.out.println(time + " \t " + action +" \t \t "+"---" +" \t "+ open+ " \t " + (state.getKassor() - state.getActiveKassor())+ " \t " + timenoqueue + " \t " + state.getCurrentCustomers() + " \t " + state.getCompletedCustomers()+" \t "+ state.getMissedCustomers()+" \t " + state.getQueueingCustomers() + " \t "+ timequeue + " \t " + state.getFIFO().getSize() +" \t "+ state.getFIFO().toString());
+			System.out.println(time + " \t " + action + " \t \t " + "---" + " \t " + open + " \t "
+					+ (state.getKassor() - state.getActiveKassor()) + " \t " + timenoqueue + " \t "
+					+ state.getCurrentCustomers() + " \t " + state.getCompletedCustomers() + " \t "
+					+ state.getMissedCustomers() + " \t " + state.getQueueingCustomers() + " \t " + timequeue + " \t "
+					+ state.getFIFO().getSize() + " \t " + state.getFIFO().toString());
 		}
 
 	}
-/**
- * Startprint
- */
+
+	/**
+	 * Startprint
+	 */
 	public void start() {
 		System.out.println("PARAMETRAR");
 		System.out.println("==========");
@@ -82,29 +100,30 @@ public class View extends SimView {
 		System.out.println("");
 		System.out.println("HÄNDELSER");
 		System.out.println("==========");
-		System.out.println("Tid \t Händelse \t Kund \t Öppet \t LedKas  LedKaT  Kunder  CKund   MKund   KKunder TKötid  KöL     Kö");
-		System.out.println("0.00" +" " + " \t Start");
+		System.out.println(
+				"Tid \t Händelse \t Kund \t Öppet \t LedKas  LedKaT  Kunder  CKund   MKund   KKunder TKötid  KöL     Kö");
+		System.out.println("0.00" + " " + " \t Start");
 	}
-/**
- * Endprint
- */
+
+	/**
+	 * Endprint
+	 */
 	public void end() {
-		double timequeue = ((double)((int)(state.getTotalQueueTime()*100)))/100;
-		double timenoqueue = ((double)((int)((state.getTotalUnqueueTime())*100)))/100;
-		double timequeueavg =((double)((int)((timequeue/state.getQueueingCustomers())*100)))/100;
-		double timenoqueueavg = ((double)((int)((timenoqueue/state.getKassor())*100)))/100;
+		double timequeue = ((double) ((int) (state.getTotalQueueTime() * 100))) / 100;
+		double timenoqueue = ((double) ((int) ((state.getTotalUnqueueTime()) * 100))) / 100;
+		double timequeueavg = ((double) ((int) ((timequeue / state.getQueueingCustomers()) * 100))) / 100;
+		double timenoqueueavg = ((double) ((int) ((timenoqueue / state.getKassor()) * 100))) / 100;
 		System.out.println("");
 		System.out.println("RESULTAT");
 		System.out.println("==========");
-		System.out.println("1) Av " + (state.getMissedCustomers()+state.getCompletedCustomers()) + " kunder behandlades " + state.getCompletedCustomers()
-				+ " medans " + state.getMissedCustomers() + " kunder missades");
+		System.out.println(
+				"1) Av " + (state.getMissedCustomers() + state.getCompletedCustomers()) + " kunder behandlades "
+						+ state.getCompletedCustomers() + " medans " + state.getMissedCustomers() + " kunder missades");
 		System.out.println("2) Total ledig tid från " + state.getKassor() + " kassor blev " + timenoqueue
 				+ ". Genomsnittlig ledig tid per kassa blev " + timenoqueueavg);
 		System.out.println("3) Total kötid för " + state.getQueueingCustomers() + " kunder blev " + timequeue
-				+ ". Genomsnittlig kötid blev " + timequeueavg) ;
+				+ ". Genomsnittlig kötid blev " + timequeueavg);
 
 	}
-
-
 
 }
