2ed378792fb2ae46658151fd13e311d452b2f936
SimonHotDog <48484552+SimonHotDog@users.noreply.github.com>
2019-03-14 14:49:31 +0100

    Update SortedSequence.java

--- src/Lab5/Queue/SortedSequence.java
+++ src/Lab5/Queue/SortedSequence.java
@@ -5,24 +5,35 @@ import Lab5.Event.*;
 import java.util.ArrayList;
 
 /**
- * Created by Tobias Heidlund on 2019-03-08.
+ * This class holds the functionality of an event queue.
+ * 
+ * @author Victor Longberg, Tobias Heidlund Simon Lundberg och Klas Mannberg
  */
 public class SortedSequence {
     private ArrayList<Event> events = new ArrayList<Event>();
-
+    /**
+     * This method gets the next event and removes it from the queue
+     * 
+     * @return an Event
+     */
     public Event getNextEvent(){
         Event e = events.get(0);
         events.remove(0);
         return e;
     }
 
+    /**
+     * This method adds an event to the queue
+     * 
+     * @param e An Event
+     */
     public void addEvent(Event e){
        double startTime = e.getStartTime();
-       if(events.size() == 0){
+       if(events.size() == 0){ //if the queue is empty, simply adds the event.
            events.add(e);
            return;
        }
-        for (int i = 0; i < events.size(); i++) {
+        for (int i = 0; i < events.size(); i++) { // Checks the queue and places the event in the correct position based on start times. 
             if(events.get(i).getStartTime()>=startTime){
                 events.add(i,e);
                 return;
@@ -30,7 +41,11 @@ public class SortedSequence {
         }
         events.add(e);
     }
-
+    /**
+     * This method checks if the the queue has a next element
+     * 
+     * @return True or False
+     */
     public boolean hasNext(){
         return events.size() != 0;
     }
